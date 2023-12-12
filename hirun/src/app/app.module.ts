8f7d9442b89343858dfa54b19ad9a092d737f29d
ZuriGE <zurigales@gmail.com>
2023-12-12 13:18:39 +0100

    Create main pages and classes

---
+++ hirun/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NewServiceComponent } from './pages/new-service/new-service.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ChatComponent,
+    CalendarComponent,
+    ProfileComponent,
+    NewServiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ hirun/src/app/models/time-frame.spec.ts
@@ -0,0 +1,7 @@
+import { TimeFrame } from './time-frame';
+
+describe('TimeFrame', () => {
+  it('should create an instance', () => {
+    expect(new TimeFrame()).toBeTruthy();
+  });
+});
