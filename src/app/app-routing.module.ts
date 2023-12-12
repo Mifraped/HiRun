38482f8c768de03a461befac1d19c4fe0072bc7a
Miguel Franco <francamente@hotmail.es>
2023-12-12 14:58:56 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NewServiceComponent } from './pages/new-service/new-service.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "calendar", component: CalendarComponent},
+  {path: "chat", component: ChatComponent},
+  {path: "login", component: LoginComponent},
+  {path: "new-service", component: NewServiceComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "register", component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
