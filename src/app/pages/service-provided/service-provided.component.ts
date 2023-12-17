0003174e908bcab35c72f281d79a0a8fd9cf333e
Charlatan2k <octavianmihai.calin@gmail.com>
2023-12-18 00:47:23 +0100

    Fix styles.css para calendario

--- src/app/pages/service-provided/service-provided.component.ts
+++ src/app/pages/service-provided/service-provided.component.ts
@@ -3,19 +3,17 @@ import { Service } from 'src/app/models/service';
 import { ServiceService } from 'src/app/shared/service.service';
 import { HeaderNavbarService } from 'src/app/shared/header-navbar.service';
 
-
 @Component({
   selector: 'app-service-provided',
   templateUrl: './service-provided.component.html',
-  styleUrls: ['./service-provided.component.css']
+  styleUrls: ['./service-provided.component.css'],
 })
 export class ServiceProvidedComponent {
-
-
-  constructor(public serviceService: ServiceService,public headerNavbarService: HeaderNavbarService) { 
-    this.headerNavbarService.showHeader=false
-    this.headerNavbarService.showNavbar=true
-
+  constructor(
+    public serviceService: ServiceService,
+    public headerNavbarService: HeaderNavbarService
+  ) {
+    this.headerNavbarService.showHeader = false;
+    this.headerNavbarService.showNavbar = true;
   }
-
 }
