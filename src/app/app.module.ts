cb11c4574e8491326554ffc7b7ea5cefe60f3210
Charlatan2k <octavianmihai.calin@gmail.com>
2023-12-20 23:33:42 +0100

    Merge de main a fn_home_no_log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -102,7 +102,6 @@ import { FiltersService } from './shared/filters.service';
     StarRatingModule.forRoot(),
     CommonModule,
     MatDialogModule,
-
     HttpClientModule,
   ],
 
