da00bad00b54aaa6ff0be781c0b36cacc28aab7c
ZuriGE <zurigales@gmail.com>
2023-12-30 09:56:37 +0100

    Merge with fn_booking: booking page, pending link with calendar and db

--- src/app/shared/timeframe.service.ts
+++ src/app/shared/timeframe.service.ts
@@ -12,8 +12,8 @@ export class TimeframeService {
   constructor(private http: HttpClient) { }
 
 
-// private url= "https://api-hi-run.vercel.app/timeframe" 
-private url = "http://localhost:3000/timeframe" 
+private url= "https://api-hi-run.vercel.app/timeframe" 
+// private url = "http://localhost:3000/timeframe" 
 
   postTimeframe(tf:TimeFrame): Observable<ResponseTimeframe> {
     return this.http.post<ResponseTimeframe>(this.url, tf );
