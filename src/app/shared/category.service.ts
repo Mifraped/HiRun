bc35ff081c95601593921729d028de7205630197
ZuriGE <zurigales@gmail.com>
2024-01-09 21:50:30 +0100

    All categories added to home slider, FAQ info added

--- src/app/shared/category.service.ts
+++ src/app/shared/category.service.ts
@@ -62,6 +62,29 @@ export class CategoryService {
     return this.http.get(`${this.url3}?id_user=${this.userService.user.id_user}`)
   }
 
+//array categorías exitentes: añadir icono en bbdd y así no hay que hacer esto?
+iconCat = [
+  {id_category: 13, title: 'abogados', icon: 'fa-solid fa-scale-balanced'},
+  {id_category: 3, title: 'albañilería', icon: 'fa-solid fa-trowel'},
+  {id_category: 4, title: 'carpintería', icon: 'fa-solid fa-hammer'},
+  {id_category: 6, title: 'carrocería', icon: 'fa-solid fa-car-side'},
+  {id_category: 12, title: 'clases', icon: 'fa-solid fa-person-chalkboard'},
+  {id_category: 14, title: 'contabilidad', icon: 'fa-solid fa-file-invoice-dollar'},
+  {id_category: 15, title: 'deporte', icon: 'fa-solid fa-table-tennis-paddle-ball'},
+  {id_category: 7, title: 'diseño web', icon: 'fa-solid fa-pen-ruler'},
+  {id_category: 2, title: 'electricidad', icon: 'fa-solid fa-plug'},
+  {id_category: 18, title: 'estética', icon: 'fa-solid fa-spa'},
+    {id_category: 1, title: 'fontanería', icon: 'fa-solid fa-faucet-drip'},
+  {id_category: 19, title: 'fotografía', icon: 'fa-solid fa-camera-retro'},
+  {id_category: 11, title: 'idiomas', icon: 'fa-solid fa-language'},
+  {id_category: 9, title: 'informática', icon: 'fa-solid fa-computer'},
+  {id_category: 17, title: 'maquillaje', icon: 'fa-solid fa-paintbrush'},
+  {id_category: 5, title: 'mecánica', icon: 'fa-solid fa-car-side'},
+  {id_category: 10, title: 'música', icon: 'fa-solid fa-music'},
+  {id_category: 16, title: 'peluquería', icon: 'fa-solid fa-scissors'},
+  {id_category: 8, title: 'programación', icon: 'fa-solid fa-laptop-code'},  
+]
+
 }
 
 
