ac84c84589dfbee4391bc286ce2a4840e1847d7b
Miguel Franco <francamente@hotmail.es>
2024-01-10 17:49:06 +0100

    funcionalidad redondeo valoraciones

--- src/app/shared/business.service.ts
+++ src/app/shared/business.service.ts
@@ -11,8 +11,8 @@ import { UserService } from './user.service';
   providedIn: 'root',
 })
 export class BusinessService {
-  private url = 'https://api-hi-run.vercel.app/';
-  // private url = 'http://localhost:3000/';
+  // private url = 'https://api-hi-run.vercel.app/';
+  private url = 'http://localhost:3000/';
 
   public logedUserBusinesses: Business[];
 
